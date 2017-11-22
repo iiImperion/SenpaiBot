2ada72765429f7181df64b77af5b20d26c5c280d
iiImperion <32373139+iiImperion@users.noreply.github.com>
2017-11-22 14:38:11 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+const Token = "MzgyODYwMDk5ODAwMTM3NzI4.DPcbWg.TLCZ0LIScyjknF34lF4WhljdVEY";
+var bot = new Discord.Client();
+var prefix = "!";
+
+bot.on("ready", function(){
+    console.log("Bot Loaded");
+});
+
+client.login(process.env.(Token))
